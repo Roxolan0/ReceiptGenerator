3878712f2a6166c5c44a59bf309e69dfa63fb761
Roxolan <roxolan@live.fr>
2015-07-17 05:17:18 +0200

    Improved ReceiptDisplay so that receipts are properly formatted and justified.

--- src/receiptgenerator/view/ReceiptDisplay.java
+++ src/receiptgenerator/view/ReceiptDisplay.java
@@ -1,6 +1,7 @@
 package receiptgenerator.view;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import receiptgenerator.business.Receipt;
@@ -8,23 +9,59 @@ import receiptgenerator.business.Receipt;
 
 public class ReceiptDisplay {
 	
-	private static final DecimalFormat priceFormat = new DecimalFormat("#0.00");
-
-	public static void printReceipt(Receipt receipt, HashMap<String, Double> priceList) {
-		
-		System.out.print("          Customer Receipt\n"
-				+ "\n"
-				+ "Item          Quantity          Cost\n"
-				+ "нннннннннннннннннннннннннннннннннннн\n");
-		for(String productName : receipt.getPurchasedProductNames()) {
-			System.out.print(productName + "               " 
-					+ receipt.getAmount(productName) + "          "
-					+ priceFormat.format(receipt.totalPriceForProduct(productName, priceList))
-					+ "\n");
+	private static final DecimalFormat PRICE_FORMAT = new DecimalFormat("#0.00");
+	private static final int ITEM_LENGTH = 21;
+	private static final int QUANTITY_LENGTH = 10;
+	private static final int COST_LENGTH = 7;
+	private static final int TOTAL_LENGTH = ITEM_LENGTH + QUANTITY_LENGTH + COST_LENGTH;
+	
+	private static final String TITLE = "Customer Receipt";
+	private static final String ITEM = "Item";
+	private static final String QUANTITY = "Quantity";
+	private static final String COST = "Cost";
+	private static final String TOTAL = "Total";
+	private static final String CONCLUSION = "Thank you for shopping at SupaMarket";
+	
+	private static String repeatChar(char repeatedChar, int length) {
+		if(length < 0) {
+			length = 0;
 		}
-		System.out.print("нннннннннннннннннннннннннннннннннннн\n"
-				+ "Total                          г" + priceFormat.format(receipt.totalPrice(priceList)) + "\n"
-				+ "\n"
-				+ "Thank you for shopping at SupaMarket\n");
+		char[] array = new char[length];
+		Arrays.fill(array, repeatedChar);
+		return new String(array);
+	}
+	
+	public static void printReceipt(Receipt receipt, HashMap<String, Double> priceList) {		
+		System.out.print(repeatChar(' ', (TOTAL_LENGTH - TITLE.length())/2) + TITLE + "\n");
+		System.out.print("\n");
+		
+		String headerLine = "";
+		headerLine += ITEM + repeatChar(' ', ITEM_LENGTH - ITEM.length());
+		headerLine += QUANTITY;
+		headerLine += repeatChar(' ', TOTAL_LENGTH - headerLine.length() - COST.length());
+		headerLine += COST;
+		System.out.print(headerLine + "\n");
+		System.out.print(repeatChar('-', TOTAL_LENGTH) + "\n");
+		
+		for(String productName : receipt.getPurchasedProductNames()) {
+			String quantity = Integer.toString(receipt.getAmount(productName));
+			String price = PRICE_FORMAT.format(receipt.totalPriceForProduct(productName, priceList));
+			String receiptLine = "";
+			receiptLine += productName + repeatChar(' ', ITEM_LENGTH - productName.length());
+			receiptLine += quantity;
+			receiptLine += repeatChar(' ', TOTAL_LENGTH - receiptLine.length() - price.length());
+			receiptLine += price;
+			System.out.print(receiptLine + "\n");
+		}
+		
+		System.out.print(repeatChar('-', TOTAL_LENGTH) + "\n");
+		
+		String totalPrice = "г" + PRICE_FORMAT.format(receipt.totalPrice(priceList));
+		System.out.print(TOTAL 
+				+ repeatChar(' ', TOTAL_LENGTH - TOTAL.length() - totalPrice.length()) 
+				+ totalPrice
+				+ "\n");
+		System.out.print("\n");
+		System.out.print(repeatChar(' ', (TOTAL_LENGTH - CONCLUSION.length())/2) + CONCLUSION + "\n");
 	}
 }

f3a9ada77cef6c143fe2f84591356938196591ee
Roxolan <roxolan@live.fr>
2015-07-17 03:47:38 +0200

    Added unit testing for totalPriceForProduct and totalPrice. Replaced assertSame with assertEquals in other tests.

--- src/receiptgenerator/testing/ReceiptTest.java
+++ src/receiptgenerator/testing/ReceiptTest.java
@@ -9,6 +9,7 @@ import receiptgenerator.business.Receipt;
  * If this was a real project, there would be a lot more tests here, testing for edge cases, errors etc.
  */
 public class ReceiptTest {
+	private static final double EPSILON = 1e-15;
 	
 	@Test
 	public void parseTillOutputTest1() {
@@ -18,9 +19,9 @@ public class ReceiptTest {
 		
 		assertEquals(testReceipt.getPurchasedProducts().size(), 2);
 		assertTrue(testReceipt.getPurchasedProducts().containsKey("Orange"));
-		assertSame(testReceipt.getPurchasedProducts().get("Orange"), 5);
+		assertEquals((int) testReceipt.getPurchasedProducts().get("Orange"), 5);
 		assertTrue(testReceipt.getPurchasedProducts().containsKey("Banana"));
-		assertSame(testReceipt.getPurchasedProducts().get("Banana"), 3);
+		assertEquals((int) testReceipt.getPurchasedProducts().get("Banana"), 3);
 	}
 
 	@Test
@@ -31,13 +32,13 @@ public class ReceiptTest {
 		
 		assertEquals(testReceipt.getPurchasedProducts().size(), 4);
 		assertTrue(testReceipt.getPurchasedProducts().containsKey("Orange"));
-		assertSame(testReceipt.getPurchasedProducts().get("Orange"), 1);
+		assertEquals((int) testReceipt.getPurchasedProducts().get("Orange"), 1);
 		assertTrue(testReceipt.getPurchasedProducts().containsKey("Tomato"));
-		assertSame(testReceipt.getPurchasedProducts().get("Tomato"), 3);
+		assertEquals((int) testReceipt.getPurchasedProducts().get("Tomato"), 3);
 		assertTrue(testReceipt.getPurchasedProducts().containsKey("Frozen pizza"));
-		assertSame(testReceipt.getPurchasedProducts().get("Frozen pizza"), 3);
+		assertEquals((int) testReceipt.getPurchasedProducts().get("Frozen pizza"), 3);
 		assertTrue(testReceipt.getPurchasedProducts().containsKey("Box of cereal"));
-		assertSame(testReceipt.getPurchasedProducts().get("Box of cereal"), 2);
+		assertEquals((int) testReceipt.getPurchasedProducts().get("Box of cereal"), 2);
 	}
 
 	@Test
@@ -48,6 +49,31 @@ public class ReceiptTest {
 		
 		assertEquals(testReceipt.getPurchasedProducts().size(), 1);
 		assertTrue(testReceipt.getPurchasedProducts().containsKey("Loaf of bread"));
-		assertSame(testReceipt.getPurchasedProducts().get("Loaf of bread"), 1);
+		assertEquals((int) testReceipt.getPurchasedProducts().get("Loaf of bread"), 1);
+	}
+	
+	@Test
+	public void totalPriceForProductTest1() {
+		String testTillOutput = "Orange|5\nBanana|3\n";
+		Receipt testReceipt = new Receipt();
+		testReceipt.parseTillOutput(testTillOutput);
+		HashMap<String, Double> testPriceList = new HashMap<String, Double>();
+		testPriceList.put("Orange", 0.4);
+		testPriceList.put("Banana", 0.12);
+		
+		assertEquals(testReceipt.totalPriceForProduct("Orange", testPriceList), 2.0, EPSILON);
+		assertEquals(testReceipt.totalPriceForProduct("Banana", testPriceList), 0.36, EPSILON);
+	}
+	
+	@Test
+	public void totalPriceTest1() {
+		String testTillOutput = "Orange|5\nBanana|3\n";
+		Receipt testReceipt = new Receipt();
+		testReceipt.parseTillOutput(testTillOutput);
+		HashMap<String, Double> testPriceList = new HashMap<String, Double>();
+		testPriceList.put("Orange", 0.4);
+		testPriceList.put("Banana", 0.12);
+		
+		assertEquals(testReceipt.totalPrice(testPriceList), 2.36, EPSILON);
 	}
 }
